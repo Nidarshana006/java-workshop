a93c30c69639bbb21e5bd6687f5e1e59f67ca71d
nidarshana006 <nidarshanamy24aid@vetias.ac.in>
2025-07-02 14:20:44 +0530

    my first program

--- src/com/vetias/java/workshop/basic/Greetings.java
+++ src/com/vetias/java/workshop/basic/Greetings.java
@@ -1,7 +1,7 @@
 
 public class Greetings {
 
-    public static void main(Strings[] args) {
+    public static void main(String[] args) {
         System.out.println("My first java program");
     }
 }
