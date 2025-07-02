8757c5b2d91b1b5d8a63499ba774c6392e55c038
nidarshana006 <nidarshanamy24aid@vetias.ac.in>
2025-07-02 14:58:45 +0530

    array using java

--- marksarray.java
+++ marksarray.java
@@ -1,8 +1,8 @@
 public class marksarray {
     public static void main(String[] args){
-        int []marks=(90,80,78,69,86);
-        for (int marks:marks) {
-            System.out.println("marks");
+        int [] marks = {90,80,78,69,86};
+        for (int mark: marks) {
+            System.out.println(mark);
         }
     }
 }
