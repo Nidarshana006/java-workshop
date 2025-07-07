f1d97eeeb6a44ff339dea76c3498d466fbc18bb2
nidarshana006 <nidarshanamy24aid@vetias.ac.in>
2025-07-07 14:24:58 +0530

    appending

---
+++ src/com/vetias/java/workshop/basic/Append.java
@@ -0,0 +1,18 @@
+package src.com.vetias.java.workshop.basic;
+
+public class Append {
+    public static void main(String[] args){
+        StringBuilder sentence = new StringBuilder();
+        sentence.append("java ");
+        sentence.append("is ");
+        sentence.append("the ");
+        sentence.append("most  ");
+        sentence.append("used ");
+        sentence.append("language ");
+        sentence.append("for ");
+        sentence.append("enterprise ");
+        sentence.append("applications ");
+        System.out.println(sentence.toString());
+    }
+}
+
