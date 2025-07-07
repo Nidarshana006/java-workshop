0babafb48ee0a3cdcbde57c981357ff9cc1b8591
nidarshana006 <nidarshanamy24aid@vetias.ac.in>
2025-07-07 10:30:44 +0530

    Stringupper

---
+++ src/com/vetias/java/workshop/basic/Stringexample.java
@@ -0,0 +1,13 @@
+package src.com.vetias.java.workshop.basic;
+
+public class Stringexample {
+    public static void main(String[] args){
+        String firstname="Nidhu";
+        String lastname="Nidarshana";
+        String Name=firstname.concat(lastname);
+        System.out.println(Name);
+        String finalname=Name.toUpperCase();
+        System.out.println(finalname);
+    }
+
+}
